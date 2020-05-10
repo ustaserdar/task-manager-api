e3bdf476ada2fb645128e06eba947e0131def6e0
Serdar Usta <dtsusta@digiturk.com.tr>
2020-05-10 16:06:27 +0300

    Swagger UI implentation completed

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+ 
 const app = express()
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json())
 app.use(userRouter)
